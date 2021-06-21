db901fce338d78b1f8c307c0c8299539aea9c80c
Ashley Kohl <akohl22@gmail.com>
2021-06-20 23:34:14 -0600

    update routes

--- routes/api.js
+++ routes/api.js
@@ -10,6 +10,26 @@ router.get("/api/workouts", (req, res) => {
         res.status(400).json(err);
     });
 });
+
+// router.get('/api/workouts', (req, res) => {
+//     Workout.aggregate([
+//       {
+//         $addFields: {
+//           totalDuration: {
+//             $sum: '$exercises.duration',
+//           },
+//         },
+//       },
+//     ])
+//       .then((dbWorkouts) => {
+//         res.json(dbWorkouts);
+//       })
+//       .catch((err) => {
+//         res.json(err);
+//       });
+//   });
+
+
 router.post("/api/workouts", (req, res) => {
     Workout.create({})
     .then(dbWorkout => {
